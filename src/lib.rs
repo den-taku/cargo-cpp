c8d1e7cd6a9b30218535a847ce44b3bd9d48097d
DenTaku <dentaku1412@gmail.com>
2021-04-30 04:52:18 +0900

    Implement some functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,51 @@
-use structopt::StructOpt;
+use structopt::clap::arg_enum;
+use structopt::{clap, StructOpt};
 
 /// Make new package for implement something in C++
 #[derive(StructOpt)]
+#[structopt(setting(clap::AppSettings::ColoredHelp))]
 pub struct Cli {
     /// what want to do
-    pub action: String,
+    pub action: Action,
+
+    /// pacage's name
+    pub name: Option<String>,
+
+    /// add source as library
+    #[structopt(short = "l", long = "library")]
+    pub lib_name: Option<String>
 }
 
+arg_enum! {
+    #[derive(Debug)]
+    pub enum Action {
+        new,
+        build,
+        run,
+        test,
+        add,
+    }
+}
+
+/// This is a template for ./main/main.cpp
 pub fn main_cpp() -> String {
-    "#include <iostream>\n// using namespace std;\n\nint main() {\n    //\n}\n".to_string()
+    "#include <iostream>\n// using namespace std;\n\nint main() {\n    std::cout << \"Hello, world\" << std::endl;\n    return 0;\n}\n".to_string()
+}
+
+/// This is a template for ./main/BUILD
+pub fn build(name: String) -> String {
+    format!(
+        "cc_binary (\n    name = \"{}\",\n    srcs = [\"{}.cpp\"]\n)",
+        name.clone(),
+        name
+    )
+}
+
+/// This is a template for ./WORKSPACE
+pub fn workspace() -> String {
+    "".to_string()
+}
+
+pub fn config(name: String) -> String {
+    format!("name = \"{}\"\nlibrary = false", name)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,53 @@
-use human_panic::setup_panic;
+// use human_panic::setup_panic;
+use anyhow::Result;
 use structopt::StructOpt;
+use cargo_cpp::Action::*;
 
-fn main() {
-    setup_panic!();
+fn main() -> Result<()> {
+    // setup_panic!();
 
     let args = cargo_cpp::Cli::from_args();
 
-    println!("{}", args.action);
-    println!("{}", cargo_cpp::main_cpp());
+    match args.action {
+        new => {
+            println!("args: new\nMake files and directories...\n");
+            println!("./main/main.cpp:\n{}", cargo_cpp::main_cpp());
+            println!("./main/BUILD:\n{}", cargo_cpp::build(args.name.clone().unwrap()));
+            println!("./WORKSPACE:\n{}", cargo_cpp::workspace());
+            println!("./config:\n{}", cargo_cpp::config(args.name.unwrap()));
+        },
+        build => {
+            println!("args: build");
+            println!("READ config");
+            println!("$ bazel build //main:{}", "filename");
+        },
+        run => {
+            println!("args: run");
+            println!("READ config");
+            println!("$ bazel run //main:{}", "filename");
+        },
+        test => {
+            println!("args: test");
+            println!("READ config");
+            println!("TEST");
+        },
+        add => {
+            println!("args: add");
+            if let Some(name) = args.lib_name {
+                println!("add statement to BUILD");
+                println!("cc_library(\n    name = \"{}\",\n    srcs = [\"{}.cpp\"]\n    hdrs = [\"{}\".h],\n)]", name, name, name);
+                println!("add statement to cc_binary in BUILD");
+                println!("    deps = [\":{}.h\",],", name);
+            } else {
+                println!("add statement to cc_binary in BUILD");
+                println!("")
+            }
+            println!("add sentence to cc_library in BUILD:");
+            println!(" hdrs = [\"filename.h\"] in cc_binary");
+        }
+    }
 
+    
+
+    Ok(())
 }
